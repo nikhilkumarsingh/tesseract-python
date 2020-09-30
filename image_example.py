0d5ce13db6cb279959eb83d825c9dc3af16d2ac5
Sukhen Karfa <karfasukhen.iem@gmail.com>
2020-10-01 02:22:12 +0530

    Update image_example.py

--- image_example.py
+++ image_example.py
@@ -3,6 +3,6 @@ import pytesseract
 
 im = Image.open("sample1.jpg")
 
-text = pytesseract.image_to_string(im, lang = 'eng')
+txt = pytesseract.image_to_string(im, lang = 'eng')
 
-print(text)
+print(txt)
